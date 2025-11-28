e316d4177549af41f08fa71f0f89f4a0846842f7
Guilherme Gomes <93097367+guilhermeeu@users.noreply.github.com>
2025-11-28 20:03:43 -0300

    feat: implemented login use case

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // TODO: do this hehec
 
 import { CreateUser } from "./application/usecases/create.usecase";
+import { LoginUser } from "./application/usecases/login.usecase";
 import { UserRepository } from "./infrastructure/database/drizzle/repositories/user.repository";
 
 const create = new CreateUser(new UserRepository)
@@ -8,3 +9,8 @@ const create = new CreateUser(new UserRepository)
 async function createUser(nickname: string, email: string, password: string) {
     await create.execute({ nickname: nickname, email: email, password: password})
 }
+
+
+const login = new LoginUser(new UserRepository)
+
+console.log(await login.execute({ email: "kronos@gmail.com", password: "123"}))
